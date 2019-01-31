f45966fc581d7cb5488a2d72e7dc498cb49a5dc6
leduardo99 <luiseduardo@computex.com.br>
2019-01-31 19:15:15 -0300

    Setup.js fixes

--- src/utils/setup.js
+++ src/utils/setup.js
@@ -3,17 +3,18 @@ const fs = require("fs")
 
 let baseConfig = fs.readFileSync("./src/utils/setup_base.txt", "utf8")
 
+init()
 
-(async function () {
-    console.log("Iniciando a configuração automática .. CTRL+C se você quiser confiruar manualmente")
+async function init() {
+    console.log("Iniciando a configuração automática. Pressione CTRL+C se você quiser confiruar manualmente\n")
 
     const token = input.question("Insira o token do seu bot: ")
-    const firebase_key = input.question("Insira a key da sua aplicação firebase: ")
-    const firebase_domain = input.question("Insira o dominio da sua aplicação firebase: ")
-    const firebase_url = input.question("Insira a url da sua aplicação firebase: ")
-    const firebase_id = input.question("Insira o id da sua aplicação firebase: ")
-    const firebase_storage = input.question("Insira o storage da sua aplicação firebase: ")
-    const firebase_sender_id = input.question("Insira o sender id da sua aplicação firebase: ")
+    const firebase_key = input.question("Insira a key da sua aplicacao firebase: ")
+    const firebase_domain = input.question("Insira o dominio da sua aplicacao firebase: ")
+    const firebase_url = input.question("Insira a url da sua aplicacao firebase: ")
+    const firebase_id = input.question("Insira o id da sua aplicacao firebase: ")
+    const firebase_storage = input.question("Insira o storage da sua aplicacao firebase: ")
+    const firebase_sender_id = input.question("Insira o sender id da sua aplicacao firebase: ")
 
     baseConfig = baseConfig
         .replace("{{DISCORD_TOKEN}}", token)
@@ -24,8 +25,7 @@ let baseConfig = fs.readFileSync("./src/utils/setup_base.txt", "utf8")
         .replace("{{FIREBASE_STORAGE}}", firebase_storage)
         .replace("{{FIREBASE_SENDER_ID}}", firebase_sender_id)
 
-    fs.writeFileSync("../.env", baseConfig)
-    console.log("Nunca passe seu token para ninguém!")
+    fs.writeFileSync("./.env", baseConfig)
+    console.log("Nunca passe seu token para ninguém!\n")
     console.log("A configuração foi finalizada com sucesso")
-    await settings.close()
-}())
+}
